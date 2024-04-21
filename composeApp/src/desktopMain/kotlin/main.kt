65a6db399a95c27172c92972f1add7fe80b51bc8
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2024-04-21 19:39:11 +0300

    Update desktopMain/main.kt
    Signed-off-by: Justas Gasparaitis <justas.gasparaitis@telesoftas.com>

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,10 +1,24 @@
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.window.ApplicationScope
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.justas.weather.app.App
 
 fun main() =
     application {
-        Window(onCloseRequest = ::exitApplication, title = "Weather") {
-            App()
-        }
+        DesktopApp()
     }
+
+@Composable
+private fun ApplicationScope.DesktopApp() {
+    Window(onCloseRequest = ::exitApplication, title = "Weather") {
+        App()
+    }
+}
+
+@Preview
+@Composable
+private fun AppPreview() {
+    App()
+}
