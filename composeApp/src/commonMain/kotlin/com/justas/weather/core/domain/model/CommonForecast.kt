d56160e49e80074a1e4e336b16d168b6873df60e
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2024-04-21 19:37:53 +0300

    Add CommonForecast.kt
    Signed-off-by: Justas Gasparaitis <justas.gasparaitis@telesoftas.com>

---
+++ composeApp/src/commonMain/kotlin/com/justas/weather/core/domain/model/CommonForecast.kt
@@ -0,0 +1,28 @@
+package com.justas.weather.core.domain.model
+
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.datetime.Instant
+
+data class CommonForecast(
+    val items: PersistentList<CommonForecastItem> = persistentListOf(),
+    val place: String = "",
+    val provider: String = "",
+    val creationDateTime: Instant? = null,
+    val isLoading: Boolean = true,
+    val errorMessage: String = "",
+)
+
+data class CommonForecastItem(
+    val airTemperature: Double? = null,
+    val cloudCover: Double? = null,
+    val conditionCode: String = "",
+    val feelsLikeTemperature: Double? = null,
+    val instant: Instant? = null,
+    val relativeHumidity: Double? = null,
+    val seaLevelPressure: Double? = null,
+    val totalPrecipitation: Double? = null,
+    val windDirection: WindDirection? = null,
+    val windGust: Double? = null,
+    val windSpeed: Double? = null,
+)
