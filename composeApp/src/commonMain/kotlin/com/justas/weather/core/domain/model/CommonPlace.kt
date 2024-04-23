2bf50d1e4cae2735c3052630e49406bc35a963c5
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2024-04-23 14:58:36 +0300

    Add CommonPlace.kt
    Signed-off-by: Justas Gasparaitis <justas.gasparaitis@telesoftas.com>

---
+++ composeApp/src/commonMain/kotlin/com/justas/weather/core/domain/model/CommonPlace.kt
@@ -0,0 +1,26 @@
+package com.justas.weather.core.domain.model
+
+data class CommonPlace(
+    val administrativeDivision: String = "",
+    val code: String = "",
+    val countryName: String = "",
+    val countryCode: String = "",
+    val name: String = "",
+    val coordinates: CommonCoordinates = CommonCoordinates()
+) {
+    companion object {
+        val VILNIUS =
+            CommonPlace(
+                administrativeDivision = "Vilniaus miesto savivaldybė",
+                code = "vilnius",
+                countryName = "Lietuva",
+                countryCode = "LT",
+                name = "Vilnius",
+                coordinates =
+                    CommonCoordinates(
+                        latitude = 54.687046,
+                        longitude = 25.282911,
+                    ),
+            )
+    }
+}
