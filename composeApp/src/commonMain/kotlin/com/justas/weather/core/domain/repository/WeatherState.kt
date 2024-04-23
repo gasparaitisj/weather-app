7ec081b929a175dd0f5b28bfcd83a5576c016f2d
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2024-04-23 14:59:20 +0300

    Rename WeatherState.kt
    Signed-off-by: Justas Gasparaitis <justas.gasparaitis@telesoftas.com>

--- composeApp/src/commonMain/kotlin/com/justas/weather/core/domain/repository/WeatherState.kt
+++ composeApp/src/commonMain/kotlin/com/justas/weather/core/domain/repository/WeatherState.kt
@@ -4,8 +4,6 @@ import com.justas.weather.core.domain.model.CommonForecast
 import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.persistentListOf
 
-data class HomeState(
+data class WeatherState(
     val forecasts: PersistentList<CommonForecast> = persistentListOf(),
-    val latLng: Pair<Double, Double> = 54.68906448528366 to 25.282551533212363,
-    val name: String = "vilnius",
 )
